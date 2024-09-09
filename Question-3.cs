0e836c3747aff63e2b7cc7957b4df528d3f84976
Laura-Garbim <laura_garbim@hotmail.com>
2024-09-09 15:19:27 -0300

    Create Question-3.cs

---
+++ Question-3.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int INDICE = 12, SOMA = 0, K = 1;
+        while (K < INDICE)
+        {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+        Console.WriteLine("O valor de SOMA é: " + SOMA);
+    }
+}
